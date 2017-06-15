58a3c89700aaa27e93028b6426884e75c0b42dc6
wjeszak <wjeszak@gmail.com>
2017-06-15 12:06:34 +0200

    Umieszczenie adresow w strukturze

--- main.cpp
+++ main.cpp
@@ -20,25 +20,14 @@
 #define WYSW3_PORT 				PORTB
 #define WYSW4_PORT 				PORTB
 
-#define WYSW1_PIN				4
-#define WYSW2_PIN				0
-#define WYSW3_PIN 				3
-#define WYSW4_PIN				4
+#define WYSW1_PIN				(1 << 4)
+#define WYSW2_PIN				(1 << 0)
+#define WYSW3_PIN 				(1 << 3)
+#define WYSW4_PIN				(1 << 4)
 
 #define WYSW_SEGMENTY_DDR 		DDRA
 #define WYSW_SEGMENTY_PORT 		PORTA
 
-#define WYSW_ZASWIEC_1 	WYSW_PORT_1 &= ~(1 << WYSW_PIN_1)
-#define WYSW_ZGAS_1 	WYSW_PORT_1 |=  (1 << WYSW_PIN_1)
-
-#define WYSW_ZASWIEC_2 	WYSW_PORT_2 &= ~(1 << WYSW_PIN_2)
-#define WYSW_ZGAS_2 	WYSW_PORT_2 |=  (1 << WYSW_PIN_2)
-
-#define WYSW_ZASWIEC_3 	WYSW_PORT_3 &= ~(1 << WYSW_PIN_3)
-#define WYSW_ZGAS_3 	WYSW_PORT_3 |=  (1 << WYSW_PIN_3)
-
-#define WYSW_ZASWIEC_4 	WYSW_PORT_4 &= ~(1 << WYSW_PIN_4)
-#define WYSW_ZGAS_4 	WYSW_PORT_4 |=  (1 << WYSW_PIN_4)
 struct Wysw
 {
 	volatile uint8_t *ddr;
@@ -47,10 +36,12 @@ struct Wysw
 };
 Wysw tab[] =
 {
-	{&WYSW1_DDR, &WYSW1_PORT, (1 << 4)}
-		};
-volatile uint8_t *adr[4] = {&PORTD, &PORTC, &PORTB, &PORTB};
- uint8_t pin[4] = {(1 << 4), (1 << 0), (1 << 3), (1 << 4)};
+	{&WYSW1_DDR, &WYSW1_PORT, WYSW1_PIN},
+	{&WYSW2_DDR, &WYSW2_PORT, WYSW2_PIN},
+	{&WYSW3_DDR, &WYSW3_PORT, WYSW3_PIN},
+	{&WYSW4_DDR, &WYSW4_PORT, WYSW4_PIN}
+};
+
 // 0 1 2 3 4 5 6 7 8 9 zgaszone wszystkie
 uint8_t tab_cyfry[11] = {0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x00, 0x10, 0x7F};
 uint8_t cyfra[4] = {tab_cyfry[0], tab_cyfry[0], tab_cyfry[0], tab_cyfry[0]};
@@ -68,11 +59,11 @@ uint8_t volatile licznik = 0;
 
 int main()
 {
-	// jako wyjscia port z tranzystorami
-	WYSW1_DDR |= (1 << WYSW1_PIN);
-	WYSW2_DDR |= (1 << WYSW2_PIN);
-	WYSW3_DDR |= (1 << WYSW3_PIN);
-	WYSW4_DDR |= (1 << WYSW4_PIN);
+	for(uint8_t i = 0; i < 4; i++)
+	{
+		*(tab[i].ddr) |= tab[i].pin;		// jako wyjscia
+		*(tab[i].port) |= tab[i].pin; 		// gaszenie wszystkich
+	}
 	// jako wyjscia port (za wyjatkiem najstarszego bitu) z segmentami a, b, c...
 	WYSW_SEGMENTY_DDR = 0b01111111;
 	// jakas maska: &0b01111111
@@ -83,13 +74,8 @@ int main()
 	TIMSK0  |= (1 << OCIE0A);
 	sei();
 // UWAGA NA NADPISANIE NAJSTARSZEGO BITU PORTA !!!!!!!!!!!!!!!!!!!!!!!
-	// gaszenie wszystkich modulow
-	*adr[0]|= pin[0];
-	*adr[1]|= pin[1];
-	*adr[2]|= pin[2];
-	*adr[3]|= pin[3];
 
-	uint8_t ktory_wyswietlacz = 0;
+	uint8_t nr_wysw = 0;
 	uint16_t l = 0;
 	while(1)
 	{
@@ -97,16 +83,18 @@ int main()
 		{
 			t_flaga_licz = 0;
 			IntToLed(l++);
-
 		}
 		if(t_flaga)
 		{
 			t_flaga = 0;
-			PORTA = cyfra[ktory_wyswietlacz];
-			*adr[ktory_wyswietlacz] &= ~pin[ktory_wyswietlacz];
-			if(ktory_wyswietlacz == 0)	*adr[3] |= pin[3]; else *adr[ktory_wyswietlacz-1] |= pin[ktory_wyswietlacz-1];
-			ktory_wyswietlacz++;
-			if(ktory_wyswietlacz == 4) ktory_wyswietlacz = 0;
+			WYSW_SEGMENTY_PORT = cyfra[nr_wysw];				// laduj cyfre do portu
+			*(tab[nr_wysw].port) &= ~tab[nr_wysw].pin;			// zaswiec modul
+			if(nr_wysw == 0)
+				*(tab[3].port) |= tab[3].pin;					// zgas modul
+			else
+				*(tab[nr_wysw-1].port) |= tab[nr_wysw-1].pin;	// zgas modul
+			nr_wysw++;
+			if(nr_wysw == 4) nr_wysw = 0;
 		}
 	}
 }
